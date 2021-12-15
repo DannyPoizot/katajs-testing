b912d06a45b4f33055eb4a24ba4f0a37295afea2
Danny Poizot <dannypoizot@pop-os.localdomain>
2021-12-15 16:06:23 +0100

    in testing progress

---
+++ 01-looping-a-triangle/looping.js
@@ -0,0 +1,7 @@
+function loop() {
+    for(let triangle = "#"; triangle.length <=7; triangle += "#"){
+        return triangle;
+        }
+}
+
+module.exports = loop();
--- 01-looping-a-triangle/test/looping.test.js
+++ 01-looping-a-triangle/test/looping.test.js
@@ -1,21 +1,11 @@
-# Looping a triangle
+const chai = require("chai");
 
-Write a loop that makes seven calls to console.log to output the following triangle:
+const assert = chai.assert;
 
-```js
-#
-##
-###
-####
-#####
-######
-#######
-```
+const l = require("../looping");
 
-It may be useful to know that you can find the length of a string by writing .length after it.
-
-```js
-let abc = "abc";
-console.log(abc.length);
-// → 3
-```
+describe("return a triangle", function(){
+    it("should return a triangle", () => {
+        assert.isFunction(l, "l is not a function");
+    });
+});
