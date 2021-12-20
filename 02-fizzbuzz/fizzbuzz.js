2a69731be92b3fa6a90e5b64c7e5b97f1aabee4e
DannyPoizot <poizot.dan@gmail.com>
2021-12-20 20:18:06 +0100

    fizzbuzz code

--- 02-fizzbuzz/fizzbuzz.js
+++ 02-fizzbuzz/fizzbuzz.js
@@ -1,2 +1,27 @@
 // Afficher les nombres de 1 à 100
 // Deux exceptions: afficher "Fizz" pour les chiffres divisibles par 3, afficher "Buzz" pour les chiffres divisibles par 5.
+// Afficher "FizzBuzz" pour les nombres divisibles à la fois par 3 et 5.
+
+modele.exports = () => {
+  // Affichage des nombres 1 à 100
+  for (let i = 1; i < 101; i++) {
+
+    // Affichage des nombres divisibles à la fois par 3 et 5
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("FizzBuzz");
+    }
+
+    // Affichage "Fizz" pour les nombres divisibles par 3
+    else if (i % 3 == 0) {
+      console.log("Fizz");
+    } else if (i % 5 == 0) {
+
+      // Affichage "Buzz" pour les nombres divisibles par 5
+      console.log("Buzz");
+
+      // On affiche les nombres restants
+    } else {
+      console.log(i);
+    }
+  }
+}
